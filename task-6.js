2b6bc1dde7039a0a4a6f2946b63ab03699bbdc6f
Mariia Stepanova <54494950+Mariia2505@users.noreply.github.com>
2020-02-04 19:44:54 +0200

    Update task-6.js

--- task-6.js
+++ task-6.js
@@ -7,6 +7,7 @@ const form = document.querySelector('input[data-length="6"]');
 
 form.addEventListener('blur', () => {
   if (form.value.length === 6) {
+    form.classList.remove('invalid')
     form.classList.add('valid')
   } else {
     form.classList.add('invalid')
